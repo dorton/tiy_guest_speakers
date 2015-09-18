49e16ec681e4ca8be8ac28bf4be055de4a0e2953
Brian Dorton <dorton@gmail.com>
2015-09-18 14:26:55 -0500

    fixes rails admin talk name on semester

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,5 +1,4 @@
 class Speaker < ActiveRecord::Base
   has_many :talks
   has_many :semesters, through: :talks
-  default_scope { order('date ASC') }
 end
--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,4 +1,11 @@
 class Talk < ActiveRecord::Base
   belongs_to :semester
   belongs_to :speaker
+
+  default_scope { order('date ASC') }
+
+def name
+  speaker.name  
+end
+
 end
