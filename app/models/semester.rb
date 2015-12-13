c7130db96621056b09bc7e89ed49836acf49e8e4
Brian Dorton <dorton@gmail.com>
2015-12-13 15:37:10 -0600

    fixes admin semester name

--- app/models/semester.rb
+++ app/models/semester.rb
@@ -2,6 +2,10 @@ class Semester < ActiveRecord::Base
   has_many :talks
   has_many :speakers, through: :talks
 
-
+def name
+  unless start_date.nil?
+    start_date.strftime('%B %Y')
+  end
+end
 
 end
